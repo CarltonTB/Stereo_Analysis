bb6a6a8f43dace532fdde5df26a0b210cb41ee3c
Carlton Brady <ctombrady@gmail.com>
2020-04-15 21:13:01 -0400

    Adding prelim example result

--- stereo_analysis_test.py
+++ stereo_analysis_test.py
@@ -9,7 +9,7 @@ class StereoAnalysisTest(unittest.TestCase):
     def test_region_based_analysis(self):
         image1 = cv2.imread('sample_images/cones/im2.png')
         image2 = cv2.imread('sample_images/cones/im6.png')
-        disparity_map = sa.region_based_analysis_SAD(image1, image2, (5, 5), 50)
+        disparity_map = sa.region_based_analysis_SAD(image1, image2, (7, 7), 50)
         # self.assertEqual(np.size(disparity_map), np.size(image1))
         # self.assertEqual(np.size(disparity_map), np.size(image2))
         # For visual debugging:
